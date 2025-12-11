55e89a5c2a80c5004de7164ec809bb285ed89bcc
fakhreddinelargou <largofakhrddin@gmail.com>
2025-12-11 17:09:38 +0100

    create register page

--- db.sql
+++ db.sql
@@ -6,3 +6,13 @@
 USE smart_wallet;
 -- GO
 
+-- TRUNCATE TABLE income;
+-- TRUNCATE TABLE expense;
+
+
+CREATE TABLE Registers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fullName VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
